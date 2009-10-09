d2c13a9054d5a2d4fa9deef3f9d8e10d1610484b
melsdoerfer <michael@elsdoerfer.com>
2009-10-09 13:44:27 +0200

    Show author in admin.

--- quotes/admin.py
+++ quotes/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from quotes.models import Quote
 
 class QuoteAdmin(admin.ModelAdmin):
-    list_display = ('slug', 'quote')
+    list_display = ('slug', 'author', 'quote',)
     ordering = ('slug',)
 
 admin.site.register(Quote, QuoteAdmin)
