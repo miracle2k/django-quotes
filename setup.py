d48c417707ccf39c903ab33b47c07d1d24795128
Michael Elsdoerfer michael@elsdoerfer.com <michael@puppetmaster.(none)>
2009-12-04 00:42:33 +0100

    Added a setup.py file so we can install via pip requirements.

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+
+setup(
+    name='django-quotes',
+    url='http://github.com/HubertD/django-quotes',
+    license='BSD',
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
